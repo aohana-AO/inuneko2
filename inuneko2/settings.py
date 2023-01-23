1ff2296c8e5a912e9d3c52a4418eeae8ce4805c1
aohana-AO <kbc21a34@stu.kawahara.ac.jp>
2023-01-23 12:04:09 +0900

    csrf変更

--- inuneko2/settings.py
+++ inuneko2/settings.py
@@ -135,4 +135,4 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CSRF_TRUSTED_ORIGINS = ['https://ac-web2.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-4942.up.railway.app/']
